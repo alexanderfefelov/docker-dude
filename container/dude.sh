b6bbd3bd5d9544ce1678918b5cfbdea6966b1928
Alexander Fefelov <alexanderfefelov@yandex.ru>
2015-05-11 06:25:58 +0300

    Fix for "Server is already active for display 1"

--- container/dude.sh
+++ container/dude.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export DISPLAY=:1
+rm -f /tmp/.X$DISPLAY-lock
 Xvfb $DISPLAY -screen 0 800x600x16 &
 # Prevents Wine from prompting for Mono and Gecko
 export WINEDLLOVERRIDES="mscoree,mshtml="
