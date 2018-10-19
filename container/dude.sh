9bef2a86ec78579f3c6049d10092a23ea673cb80
Luciano Santos <lucianosds@gmail.com>
2018-10-19 19:15:50 -0300

    small sugestion to display
    800x600 is to small. just making a sugestion to configuring as 1024x768x16.

--- container/dude.sh
+++ container/dude.sh
@@ -2,7 +2,7 @@
 
 export DISPLAY=:1
 rm -f /tmp/.X1-lock
-Xvfb :1 -screen 0 800x600x16 &
+Xvfb :1 -screen 0 1024x768x16 &
 # Prevents Wine from prompting for Mono and Gecko
 export WINEDLLOVERRIDES="mscoree,mshtml="
 wine 'z:\dude\dude.exe' --server
